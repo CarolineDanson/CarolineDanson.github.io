bb91065a240f89db3490e768539684113cbebbbb
Caroline <60854373+CarolineDanson@users.noreply.github.com>
2020-05-15 10:12:20 +0200

    Contact form

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -1,97 +1,68 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
-  render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
+   constructor(props) {
+      super(props);
+      this.submitForm = this.submitForm.bind(this);
+      this.state = {
+        status: ""
+      }
     }
 
-    return (
-      <section id="contact">
+   render() {
 
-         <div className="row section-head">
+      const { status } = this.state;
 
-            <div className="two columns header-col">
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var street = this.props.data.address.street;
+         var city = this.props.data.address.city;
+         var state = this.props.data.address.state;
+         var zip = this.props.data.address.zip;
+         var phone = this.props.data.phone;
+         var email = this.props.data.email;
+         var message = this.props.data.contactmessage;
+      }
 
-               <h1><span>Get In Touch.</span></h1>
+      return (
+         <section id="contact">
+            <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/mbjzlepr"
+        method="POST"
+      >
+       
+        <label>Email:</label>
+        <input type="email" name="email" />
+        <label>Message:</label>
+        <input type="text" name="message" />
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error. Try again!</p>}
+      </form>
+         </section >
+      );
+   }
 
-            </div>
-
-            <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-
-            </div>
-
-         </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-
-            </aside>
-      </div>
-   </section>
-    );
-  }
+   submitForm(ev) {
+      ev.preventDefault();
+      const form = ev.target;
+      const data = new FormData(form);
+      const xhr = new XMLHttpRequest();
+      xhr.open(form.method, form.action);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+          form.reset();
+          this.setState({ status: "SUCCESS" });
+        } else {
+          this.setState({ status: "ERROR" });
+        }
+      };
+      xhr.send(data);
+    }
+   
 }
 
 export default Contact;
